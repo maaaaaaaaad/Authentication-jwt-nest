68108ca0c07364d76c640301f4fd7b642c30c763
DONG WOONG OH <integral5137@gmail.com>
2021-07-21 00:05:18 +0900

    add to auth service logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User, UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(
+    username: string,
+    inputPassword: string,
+  ): Promise<Omit<User, 'password'>> {
+    //
+    const user = await this.usersService.findOne(username);
+
+    if (user && user.password === inputPassword) {
+      const { password, ...result } = user;
+      console.log(password);
+
+      return result;
+    }
+
+    return null;
+  }
+}
